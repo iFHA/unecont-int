64ad9a1894b6f4f93f719428af6bcf47f8a5b595
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2025-01-27 22:53:56 -0300

    Add MudBlazor

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Web;
+using Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("API", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+builder.Services.AddScoped<DocumentoAPI>();
+builder.Services.AddMudServices();
+
 
 await builder.Build().RunAsync();
