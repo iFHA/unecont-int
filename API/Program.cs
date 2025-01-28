b7d6c31a0b4e297d6c69a3c0ff9527f62dd932d3
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2025-01-27 22:51:56 -0300

    Enabling cors

--- API/Program.cs
+++ API/Program.cs
@@ -23,7 +23,19 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https:/
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(
+    options => options.AddPolicy(
+        "wasm",
+        policy => policy.WithOrigins([builder.Configuration["BackendUrl"] ?? "http://localhost:5260",
+            builder.Configuration["FrontendUrl"] ?? "http://localhost:5081"])
+            .AllowAnyMethod()
+            .SetIsOriginAllowed(pol => true)
+            .AllowAnyHeader()
+            .AllowCredentials()));
+
 var app = builder.Build();
+
+app.UseCors("wasm");
 
 app.AddEndpointsDocumentos();
 
